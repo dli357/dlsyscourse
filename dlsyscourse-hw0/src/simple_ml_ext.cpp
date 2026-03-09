8d1f27c3f9d84f486cecea52e3b0421108e2946d
unknown <23002167+dli357@users.noreply.github.com>
2026-03-08 22:00:51 -0700

    hw0: Initial epoch logic

--- dlsyscourse-hw0/src/simple_ml_ext.cpp
+++ dlsyscourse-hw0/src/simple_ml_ext.cpp
@@ -5,6 +5,14 @@
 
 namespace py = pybind11;
 
+float dot_product(float* x, float* y, size_t n) {
+    float acc = 0;
+    for (int i = 0; i < n; i++) {
+        acc += x[i] * y[i];
+    }
+    return acc;
+}
+
 
 void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
 								  float *theta, size_t m, size_t n, size_t k,
@@ -33,7 +41,54 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
      */
 
     /// BEGIN YOUR CODE
-
+    // Randomly shuffle samples into batches
+    size_t* shuffled_indices = new size_t[m];
+    for (size_t i = 0; i < m; i++) {
+        shuffled_indices[i] = i;
+    }
+    srand (time(NULL));
+    for (size_t i = 0; i < m; i++) {
+        size_t rand_ind = rand() % (m - i);
+        size_t tmp = shuffled_indices[rand_ind];
+        shuffled_indices[rand_ind] = shuffled_indices[i];
+        shuffled_indices[i] = tmp;
+    }
+    // Loop through each batch.
+    size_t num_batches = m / batch;
+    for (size_t i = 0; i < num_batches; i++) {
+        size_t batch_base_ind = i * batch * n;
+        // Loop through each example within a single batch.
+        std::vector<float> gradients;
+        gradients.reserve(ki);
+        for (size_t j = 0; j < batch; j++) {
+            size_t item_ind = batch_base_ind + j;
+            std::vector<float> intermediates;
+            intermediates.reserve(ki);
+            float total_softmax = 0;
+            // Compute individual softmax for each class K
+            for (size_t ki = 0; ki < k; ki++) {
+                float softmax_i = std::exp(dot_product(x + item_ind, theta + ki, n));
+                total_softmax += softmax_i;
+                intermediates.push_back(softmax_i);
+            }
+            for (size_t ki = 0; ki < k; ki++) {
+                intermediates[ki] /= total_softmax;
+            }
+            // Compute cost based on resulting vs expected
+            float total_cost = 0;
+            for (size_t ki = 0; ki < k; ki++) {
+                float cost_base = 0;
+                if (y[item_ind] == ki) {
+                    cost_base = 1;
+                }
+                gradients[ki] += cost_base - intermediates[ki];
+            }
+        }
+        // Average the gradients for the minibatch.
+        for (size_t ki = 0; ki < k; ki++) {
+            theta[ki] -= gradients[ki] / batch * lr;
+        }
+    }
     /// END YOUR CODE
 }
 
